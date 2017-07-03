57e979b5a864752004de2b9f2e1471d39583bfa8
RavenVL <ravenvl71@gmail.com>
2017-07-03 11:06:14 +0300

    3

--- input.php
+++ input.php
@@ -2,20 +2,17 @@
 //$rez = $_POST['data'] . '   '.$_POST['cur'];
 //echo $rez;
 $data = $_POST['data'];
-if (!isset($data)){
-   $rezArray = array();
-    $xml = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp' . "?date_req=$data");
-    $sxml = simplexml_load_string($xml);
+$xml = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp' . "?date_req=$data");
+$sxml = simplexml_load_string($xml);
+if (!isset($data)) {
+    $rezArray = array();
     foreach ($sxml as $item) {
-     $rezArray[] = $item->CharCode;
+        $rezArray[] = $item->CharCode;
     }
     echo json_encode($rezArray);
-}
-else{
-    $xml = file_get_contents('http://www.cbr.ru/scripts/XML_daily.asp' . "?date_req=$data");
-    $sxml = simplexml_load_string($xml);
+} else {
     foreach ($sxml as $item) {
-        if ($item->CharCode== $_POST['cur']){
+        if ($item->CharCode == $_POST['cur']) {
             echo $item->Value;
             exit();
 
